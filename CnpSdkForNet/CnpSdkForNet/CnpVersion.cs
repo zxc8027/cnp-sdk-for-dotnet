e0a74b95b62f4b089834849a9a3226f0fb13c77a
zxc8027 <zxc8027@g.rit.edu>
2019-12-20 09:29:40 -0500

    Incremented SDK version

--- CnpSdkForNet/CnpSdkForNet/CnpVersion.cs
+++ CnpSdkForNet/CnpSdkForNet/CnpVersion.cs
@@ -4,13 +4,11 @@
  * Stores the current version of the SDK.
  */
 
-using Cnp.Sdk.VersionedXML;
-
 namespace Cnp.Sdk
 {
     public class CnpVersion
     {
         public const string CurrentCNPXMLVersion = "12.10";
-        public const string CurrentCNPSDKVersion = "DotNet;" + CurrentCNPXMLVersion + ".3";
+        public const string CurrentCNPSDKVersion = "DotNet;" + CurrentCNPXMLVersion + ".4";
     }
 }
